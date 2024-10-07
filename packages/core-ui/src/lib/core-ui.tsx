34dee08ee38fce794048aa0113d3155abb264f84
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-10-07 13:58:31 +0200

    feat: add core-ui package

---
+++ packages/core-ui/src/lib/core-ui.tsx
@@ -0,0 +1,11 @@
+import styles from './core-ui.module.scss';
+
+export function CoreUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to CoreUi!</h1>
+    </div>
+  );
+}
+
+export default CoreUi;
