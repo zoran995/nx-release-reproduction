9619f642407f09dfb34e53899afe39e95ed23711
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-10-07 13:56:16 +0200

    feat: add sdk library

--- apps/nx-release-reproduction/src/app/app.tsx
+++ apps/nx-release-reproduction/src/app/app.tsx
@@ -1,9 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { sdk } from '@zoran995/sdk';
+
 import styles from './app.module.scss';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  sdk();
+
   return (
     <div>
       <NxWelcome title="nx-release-reproduction" />
