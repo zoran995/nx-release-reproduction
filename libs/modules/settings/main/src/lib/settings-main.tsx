9f20de61d8f4ba5289c6f6c527520df37b13850d
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-10-07 14:02:13 +0200

    feat: add first module

---
+++ libs/modules/settings/main/src/lib/settings-main.tsx
@@ -0,0 +1,11 @@
+import styles from './settings-main.module.scss';
+
+export function SettingsMain() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SettingsMain!</h1>
+    </div>
+  );
+}
+
+export default SettingsMain;
